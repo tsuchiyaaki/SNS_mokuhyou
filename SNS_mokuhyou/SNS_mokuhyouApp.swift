2d30127238f61ceeb2dab35e41caf221bdf3521d
tsuchiyaaki <a.tsuchiya.d.f@gmail.com>
2026-06-06 20:02:49 +0900

    First Commit

--- SNS_mokuhyou/SNS_mokuhyouApp.swift
+++ SNS_mokuhyou/SNS_mokuhyouApp.swift
@@ -25,7 +25,7 @@ struct SNS_mokuhyouApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(Date: "")
         }
         .modelContainer(sharedModelContainer)
     }
